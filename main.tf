948ebec3e96498968db49e198ed3e2bd45337974
jalcalaroot <johanalcala104@gmial.com>
2021-03-21 14:16:34 -0300

    quitando tags

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support = true
 
-  tags {
-     Name = "main"
-     env = "terraform"
-  }
+#  tags {
+#     Name = "main"
+#     env = "terraform"
+#  }
 }
 #-----------------------------------------------------------
 #.......................
